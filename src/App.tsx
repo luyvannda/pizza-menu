e6feab91ed6d714cb6bebd4af9073d87c6f25482
luyvannda <luyvannda@gmail.com>
2024-02-15 09:19:11 +0700

    fix lint stage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-// import { useState } from 'react'
-import './App.css';
+import "./App.css";
+import Pizza from "./components/Pizza";
 
 function App() {
   return (
     <div>
       <h1 className="text-2xl font-semibold underline">Hello World</h1>
+      <Pizza />
     </div>
   );
 }
---
+++ src/components/Pizza.tsx
@@ -0,0 +1,7 @@
+export default function Pizza() {
+  return (
+    <div>
+      <h2>Pizza</h2>
+    </div>
+  );
+}
