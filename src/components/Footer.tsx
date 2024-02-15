47e9b812c508cbc0c5264ee127f640c892f4c650
luyvannda <luyvannda@gmail.com>
2024-02-15 10:32:27 +0700

    add example of logic to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,10 @@
 export default function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
   return (
     <footer>
       {new Date().toLocaleTimeString()}. We&apos;re currently open
