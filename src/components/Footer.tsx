cbbfe214787d773f4d32e587629e18a64fa16784
luyvannda <luyvannda@gmail.com>
2024-02-25 10:58:57 +0700

    add conditional rendering to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from "react";
+
 export default function Footer() {
   const hour = new Date().getHours();
   const openHour = 8;
@@ -5,9 +7,26 @@ export default function Footer() {
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
 
+  const conditionMessage = (): ReactNode => {
+    return isOpen ? (
+      <p>
+        {new Date().toLocaleTimeString()}. We&apos;re open until {closeHour}
+        :00. Come visit us or order online.
+      </p>
+    ) : (
+      <p>
+        {new Date().toLocaleTimeString()}. Sorry, We&apos;re currently close.
+        Please come back between {openHour}:00 - {closeHour}:00.
+      </p>
+    );
+  };
+
   return (
     <footer className="text-[1.4rem]">
-      {new Date().toLocaleTimeString()}. We&apos;re currently open
+      <div className="flex flex-col items-center gap-[2.4rem]">
+        {conditionMessage()}
+        {isOpen && <button className="btn">Order</button>}
+      </div>
     </footer>
   );
 }
