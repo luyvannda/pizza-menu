53c287b11768922b384c8e64f24993ebca7cd2df
luyvannda <luyvannda@gmail.com>
2024-02-28 11:45:50 +0700

    conditionally add class to pizza that is soldout

--- src/components/Pizza.tsx
+++ src/components/Pizza.tsx
@@ -13,19 +13,20 @@ interface PizzaProps {
 const Pizza: FC<PizzaProps> = ({
   pizzaObj: { id, name, ingredients, price, photoName, soldOut },
 }) => {
-  if (soldOut) return null;
-
   return (
-    <li key={id} className="flex gap-[3.2rem]">
+    <li key={id} className={`flex gap-[3.2rem] ${soldOut && "text-[#888]"}`}>
       <img
-        className="h-48 w-48 self-start object-cover object-center"
+        className={`h-48 w-48 self-start object-cover object-center ${soldOut && "opacity-80 grayscale"}`}
         src={photoName}
         alt={name}
       />
       <div className="flex flex-col gap-3 py-2">
         <h3 className="text-[2rem] font-normal">{name}</h3>
         <p className="mb-auto text-[1.4rem] font-light italic">{ingredients}</p>
-        <span className="block text-2xl">${price}</span>
+
+        <span className="block text-2xl">
+          {soldOut ? "Sold Out" : `$${price}`}
+        </span>
       </div>
     </li>
   );
