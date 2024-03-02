5ce8f574c202fb7db25273a4cfbfb40a604f65ed
luyvannda <luyvannda@gmail.com>
2024-03-02 09:27:33 +0700

    add image url link

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,28 @@ function App() {
   const title = "Pizza Menu";
   const description =
     "A pizza menu project built with react, typescript and tailwind!";
-  const imageUrl = "#";
+  const imageOg =
+    "https://onedrive.live.com/embed?resid=FF8AC3081FC56D69%2188585&authkey=%21AIcJp8wM2mNNcMI&width=1200&height=630";
+
+  const imageTwitter =
+    "https://onedrive.live.com/embed?resid=FF8AC3081FC56D69%2188585&authkey=%21AIcJp8wM2mNNcMI&width=600&height=300";
 
   return (
     <>
       <Helmet>
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content={imageUrl} />
-        <meta property="og:url" content="https://www.yourwebsite.com" />
+        <meta property="og:image" content={imageOg} />
+        <meta
+          property="og:url"
+          content="https://luyvannda.github.io/pizza-menu/"
+        />
         <meta property="og:type" content="website" />
-        {/* Twitter Card */}
+
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={imageUrl} />
+        <meta name="twitter:image" content={imageTwitter} />
         <meta name="twitter:site" content="@your_twitter_handle" />
         <title>{title}</title>
       </Helmet>
