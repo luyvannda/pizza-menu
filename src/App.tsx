c8a26b558827f913712d04eb2c88488784244509
luyvannda <luyvannda@gmail.com>
2024-03-02 09:17:15 +0700

    add react-helmet for url preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
+import { Helmet } from "react-helmet";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 
 function App() {
+  const title = "Pizza Menu";
+  const description =
+    "A pizza menu project built with react, typescript and tailwind!";
+  const imageUrl = "#";
+
   return (
-    <div className="flex flex-col items-center gap-12 overflow-hidden md:gap-28">
-      <Header />
-      <Menu />
-      <Footer />
-    </div>
+    <>
+      <Helmet>
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={imageUrl} />
+        <meta property="og:url" content="https://www.yourwebsite.com" />
+        <meta property="og:type" content="website" />
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={imageUrl} />
+        <meta name="twitter:site" content="@your_twitter_handle" />
+        <title>{title}</title>
+      </Helmet>
+
+      <div className="flex flex-col items-center gap-12 overflow-hidden md:gap-28">
+        <Header />
+        <Menu />
+        <Footer />
+      </div>
+    </>
   );
 }
 
